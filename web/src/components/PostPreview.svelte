ca3b43caf13416178f211948f6e307343e64e2ba
Simon Jespersen <simjes91@me.com>
2019-07-30 15:29:12 +0200

    Initial post preview component

---
+++ web/src/components/PostPreview.svelte
@@ -0,0 +1,80 @@
+<script>
+  import { fly } from "svelte/transition";
+
+  export let post;
+  let visible = false;
+
+  function onMouseOver() {
+    visible = true;
+  }
+
+  function onMouseOut() {
+    visible = false;
+  }
+</script>
+
+<style>
+  .card {
+    height: 100%;
+    width: 100%;
+    position: relative;
+    overflow: hidden;
+    background: linear-gradient(30deg, #ff0033, #00ff00);
+  }
+
+  .post-info {
+    color: white;
+    background: #111111cc;
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 100px;
+  }
+
+  .card:hover > .post-image {
+    animation: image-up 1000ms cubic-bezier(0.215, 0.61, 0.355, 1) forwards;
+  }
+
+  .post-image {
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    object-fit: cover;
+    animation: image-down 500ms ease-out forwards;
+  }
+
+  @keyframes image-up {
+    from {
+      transform: translateY(0);
+    }
+    to {
+      transform: translateY(-50px);
+    }
+  }
+
+  @keyframes image-down {
+    from {
+      transform: translateY(-50px);
+    }
+    to {
+      transform: translateY(0);
+    }
+  }
+</style>
+
+<a
+  on:mouseover={onMouseOver}
+  on:mouseout={onMouseOut}
+  rel="prefetch"
+  href="blog/{post.slug}"
+  title={post.title}>
+  <div class="card">
+    <img class="post-image" src={post.image} alt={post.title} />
+
+    {#if visible}
+      <div transition:fly={{ y: 100, duration: 1000 }} class="post-info">
+        {post.title}
+      </div>
+    {/if}
+  </div>
+</a>
--- web/src/routes/blog/index.svelte
+++ web/src/routes/blog/index.svelte
@@ -1,39 +1,66 @@
 <script context="module">
-  import client from '../../sanityClient'
-	export function preload({ params, query }) {
-    return client.fetch('*[_type == "post" && defined(slug.current) && publishedAt < now()]|order(publishedAt desc)').then(posts => {
-			return { posts };
-		}).catch(err => this.error(500, err));
-	}
+  import PostPreview from "../../components/PostPreview.svelte";
+  import client from "../../sanityClient";
+  export function preload({ params, query }) {
+    return client
+      .fetch(
+        `*[_type == "post" && defined(slug.current) && publishedAt < now()]
+				{
+					title,
+					"slug": slug.current,
+					"image": mainImage.asset->url
+				}
+				| order(publishedAt desc)`
+      )
+      .then(posts => {
+        return { posts };
+      })
+      .catch(err => this.error(500, err));
+  }
 </script>
 
 <script>
   export let posts;
 
   function formatDate(date) {
-    return new Date(date).toLocaleDateString()
+    return new Date(date).toLocaleDateString();
   }
 </script>
 
 <style>
-	ul {
-		margin: 0 0 1em 0;
-		line-height: 1.5;
-	}
+  ul {
+    margin: 0;
+    padding: 0;
+    display: grid;
+    grid-gap: 20px;
+    grid-template-columns: repeat(auto-fill, 300px);
+    justify-content: center;
+  }
+
+  li {
+    list-style: none;
+    height: 300px;
+    width: 300px;
+  }
 </style>
 
 <svelte:head>
-	<title>Blog</title>
+  <title>Blog</title>
 </svelte:head>
 
 <h1>Recent posts</h1>
 
 <ul>
-	{#each posts as post}
-		<!-- we're using the non-standard `rel=prefetch` attribute to
+  {#each posts as post}
+    <!-- we're using the non-standard `rel=prefetch` attribute to
 				tell Sapper to load the data for the page as soon as
 				the user hovers over the link or taps it, instead of
 				waiting for the 'click' event -->
-		<li><a rel='prefetch' href='blog/{post.slug.current}'>{post.title}</a> ({formatDate(post.publishedAt)})</li>
-	{/each}
+    <li>
+      <!-- <a rel="prefetch" href="blog/{post.slug.current}">{post.title}</a>
+      ({formatDate(post.publishedAt)}) -->
+      <PostPreview {post} />
+    </li>
+  {/each}
 </ul>
+<div>{JSON.stringify(posts)}</div>
